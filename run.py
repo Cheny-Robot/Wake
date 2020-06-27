3f75d0f583153b5fefc1b5585406bd076d47fa27
CharlieYu <charlieyu4994@outlook.com>
2020-06-27 10:55:27 +0800

    脑抽一直有

--- run.py
+++ run.py
@@ -6,5 +6,6 @@ i = 0
 urls = sys.argv[1].split(',') if len(sys.argv) >= 2 else ['https://www.antmoe.com/']
 for url in urls:
     req = requests.get(url)
-    print(f'第{i+=1}号网址唤醒状态:', req, time.strftime(
+    print(f'第{i}号网址唤醒状态:', req, time.strftime(
         '%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    i = i+1
