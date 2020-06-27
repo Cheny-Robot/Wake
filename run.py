5494f15572e86f4a8fc4d455842761d561a103ff
CharlieYu <charlieyu4994@outlook.com>
2020-06-27 10:45:05 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,11 +2,8 @@ import requests
 import sys
 import time
 
-if (len(sys.argv) >= 2):
-    urls = sys.argv[1].split(',')
-else:
-    urls = ['https://www.antmoe.com/']
-for i in range(0, len(urls)):
-    req = requests.get(urls[i])
+urls = sys.argv[1].split(',') if len(sys.argv) >= 2 else ['https://www.antmoe.com/']
+for url in urls:
+    req = requests.get(url)
     print(f'第{i}号网址唤醒状态:', req, time.strftime(
         '%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
